0bb57b2af5ac049382606feb2122873a3337cba1
jayasimha <jayasimhajay1994@gmail.com>
2018-03-01 18:54:14 +0530

    Interfaces and Abstract classes

---
+++ AbstractClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOApplication
+{
+    //Creating an Abstract Class
+    public abstract class absClass
+    {
+        //A Non abstract method
+        public int AddTwoNumbers(int Num1, int Num2)
+        {
+            return Num1 + Num2;
+        }
+
+        //An abstract method, to be
+        //overridden in derived class
+        public abstract int MultiplyTwoNumbers(int Num1, int Num2);
+    }
+    class AbstractClass: absClass
+    {
+        static void Main(string[] args)
+        {
+            //You can create an
+            //instance of the derived class
+            AbstractClass calculate = new AbstractClass();
+            int added = calculate.AddTwoNumbers(10, 20);
+            int multiplied = calculate.MultiplyTwoNumbers(10, 20);
+            Console.WriteLine("Added : {0}, Multiplied: {1}", added, multiplied);
+            Console.Read();
+        }
+        /// <summary>
+        /// using override keyword,
+        /// implementing the abstract method
+        /// MultiplyTwoNumbers
+        /// </summary>
+        /// <param name="Num1"></param>
+        /// <param name="Num2"></param>
+        /// <returns></returns>
+        public override int MultiplyTwoNumbers(int Num1, int Num2)
+        {
+            return Num1 * Num2;
+        }
+    }
+}
---
+++ TransactionsByInterface.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOApplication
+{
+    class TransactionsByInterface
+    {
+        /// <summary>
+        /// it provides the methods for any implemented classes
+        /// so that all methods can be implemented according to their need.
+        /// </summary>
+        public interface ITransactions
+        {
+            /// <summary>
+            /// methods to get the transaction information.
+            /// </summary>
+            void showTransaction();
+            double getAmount();
+        }
+        public class Transaction : ITransactions
+        {
+                private static readonly log4net.ILog log
+                = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            /// <summary>
+            /// fields required for every transaction.
+            /// </summary>
+            private string tCode;
+            private string date;
+            private double amount;
+            public Transaction(string c, string d, double a)
+            {
+                tCode = c;
+                date = d;
+                amount = a;
+            }
+            /// <summary>
+            /// This method is implemented to return the amount.
+            /// </summary>
+            /// <returns></returns>
+            public double getAmount()
+            {
+                return amount;
+            }
+            /// <summary>
+            /// This method is implemented to know the transaction information.
+            /// </summary>
+            public void showTransaction()
+            {
+                log.InfoFormat("Transaction: {0}", tCode);
+                log.InfoFormat("Date: {0}", date);
+                log.InfoFormat("Amount: {0}", getAmount());
+            }
+        }
+        /// <summary>
+        /// This class is used for instantiating and utilizing the implemented
+        /// methods of the Transaction class
+        /// </summary>
+        class TransactionByInterface
+        {
+            /// <summary>
+            /// Entry point for the project.
+            /// </summary>
+            /// <param name="args"></param>
+            static void Main(string[] args)
+            {
+                Transaction t1 = new Transaction("001", "8/10/2012", 78900.00);
+                Transaction t2 = new Transaction("002", "9/10/2012", 451900.00);
+                t1.showTransaction();
+                t2.showTransaction();
+                Console.ReadKey();
+            }
+        }
+    }
+}
